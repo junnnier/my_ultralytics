c0da4a7999dc4d8f86dbdb52a4d50c62253e7044
tj <wantianjun@zerozero.cn>
2026-01-15 04:54:38 -0100

    增加tracker测试启动文件。

---
+++ tracker_test.py
@@ -0,0 +1,43 @@
+# from ultralytics import YOLO
+
+# # Load the model and run the tracker with a custom configuration file
+# model = YOLO("yolo11n.pt")
+# results = model.track(source="https://youtu.be/LNwODJXcvt4", tracker="bytetrack.yaml")
+
+
+import cv2
+
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
+model.model.model[-1].to_onnx = False
+# Open the video file
+video_path = "HOVER_X1PROMAX_0002.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        # cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        # if cv2.waitKey(1) & 0xFF == ord("q"):
+        #     break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -481,7 +481,7 @@ class Classify(nn.Module):
         super().__init__()
         c_ = 1280  # efficientnet_b0 size
         self.conv = Conv(c1, c_, k, s, p, g)
-        self.pool = nn.AdaptiveAvgPool2d(1)  # to x(b,c_,1,1)
+        self.pool = nn.AdaptiveAvgPool2d(1)  # 自适应平均池化层 to x(b,c_,1,1)
         self.drop = nn.Dropout(p=0.0, inplace=True)
         self.linear = nn.Linear(c_, c2)  # to x(b,c2)
 
